89e12afa40afa99bd678acf2570c3ad5774dfed4
Natasha Godwin <natashasgodwin@gmail.com>
2021-07-12 15:02:11 -0400

    Initial Files

---
+++ 06 - TimeTravel/TimeTravel/TimeTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTravelApp.swift
+//  TimeTravel
+//
+//  Created by Natasha Godwin on 7/12/21.
+//
+
+import SwiftUI
+
+@main
+struct TimeTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
