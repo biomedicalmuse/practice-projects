5cb2bfed0a09d3c835ed09cf11f2c1351fe85620
Natasha Godwin <natashasgodwin@gmail.com>
2021-07-14 17:01:26 -0400

    Basic UI with @State and @Binding

--- 07 - DataFlow/DataFlow/ContentView.swift
+++ 07 - DataFlow/DataFlow/ContentView.swift
@@ -7,15 +7,57 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ProgressBar: View {
+	@Binding var value: Float
+	var body: some View {
+		GeometryReader { geometry in
+			ZStack(alignment: .leading) {
+				Rectangle()
+					.frame(width: geometry.size.width, height: geometry.size.height)
+					.opacity(0.5)
+					.foregroundColor(Color(UIColor.systemTeal))
+				Rectangle()
+					 .frame(width: min(CGFloat(self.value)*geometry.size.width, geometry.size.width), height: geometry.size.height)
+					 .animation(.linear)
+					 .foregroundColor(.blue)
+			}
+			.cornerRadius(50)
+		}
+	}
+}
+
+struct UpdateProgressView: View {
+	@State private var progress: Float = 0.0
+	var body: some View {
+		HStack {
+			ProgressBar(value: $progress)
+				.frame(height: 10)
+			Spacer()
+			Stepper("Progress", value: $progress, in: 0...100, step: 0.1)
+				.labelsHidden()
+		}
+	}
+}
+
+struct ListView: View {
+	let books = ["Complexity: A Guided Tour", "Chaos", "Network Medicine", "A First Course In Network Science", "Liminal Thinking"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+		NavigationView {
+			List {
+				ForEach(books, id: \.self) { book in
+					VStack(alignment: .leading) {
+						Text(book)
+						UpdateProgressView()
+					}
+				}
+			}
+			.navigationTitle("Books")
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+		ListView()
     }
 }
