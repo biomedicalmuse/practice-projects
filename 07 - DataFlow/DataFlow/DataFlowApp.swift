f46b261da81884afe1cc9da4ba0af67c24b9360b
Natasha Godwin <natashasgodwin@gmail.com>
2021-07-14 16:19:20 -0400

    Initial Files

---
+++ 07 - DataFlow/DataFlow/DataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataFlowApp.swift
+//  DataFlow
+//
+//  Created by Natasha Godwin on 7/14/21.
+//
+
+import SwiftUI
+
+@main
+struct DataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
