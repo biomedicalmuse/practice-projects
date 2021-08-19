3c339c9d19606398b4c285c6d43f74f5406f82f2
Natasha Godwin <natashasgodwin@gmail.com>
2021-08-19 15:02:11 -0400

    Initial Files

---
+++ 09 - DynamicList/DynamicList/DynamicListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicListApp.swift
+//  DynamicList
+//
+//  Created by Natasha Godwin on 8/19/21.
+//
+
+import SwiftUI
+
+@main
+struct DynamicListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
