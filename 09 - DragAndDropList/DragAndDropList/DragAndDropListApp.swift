d3bd77113a6169f04e4d2990db2036e8c72bff8e
Natasha Godwin <natashasgodwin@gmail.com>
2021-08-19 11:58:43 -0400

    Initial Files

---
+++ 09 - DragAndDropList/DragAndDropList/DragAndDropListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragAndDropListApp.swift
+//  DragAndDropList
+//
+//  Created by Natasha Godwin on 8/19/21.
+//
+
+import SwiftUI
+
+@main
+struct DragAndDropListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
