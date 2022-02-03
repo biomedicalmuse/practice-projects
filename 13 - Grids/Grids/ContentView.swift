c174dd4b20a25563ac9fa09ffd1d5f6da60267b6
Natasha Godwin <natashasgodwin@gmail.com>
2022-02-03 09:17:48 -0500

    Initial Files

---
+++ 13 - Grids/Grids/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Grids
+//
+//  Created by Natasha Godwin on 2/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 13 - Grids/Grids/GridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridsApp.swift
+//  Grids
+//
+//  Created by Natasha Godwin on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct GridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
