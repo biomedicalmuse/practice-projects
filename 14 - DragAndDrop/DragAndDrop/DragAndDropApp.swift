55326332371d307d813a71bba378047b1dfe8f88
Natasha Godwin <natashasgodwin@gmail.com>
2022-02-11 15:30:17 -0500

    Initial Files

---
+++ 14 - DragAndDrop/DragAndDrop/DragAndDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragAndDropApp.swift
+//  DragAndDrop
+//
+//  Created by Natasha Godwin on 2/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DragAndDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
