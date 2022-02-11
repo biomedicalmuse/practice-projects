0f9cc02e023b99acb113eef843cd5000f6d38f3b
Natasha Godwin <natashasgodwin@gmail.com>
2022-02-11 16:47:11 -0500

    Added basic drag example

--- 14 - DragAndDrop/DragAndDrop/ContentView.swift
+++ 14 - DragAndDrop/DragAndDrop/ContentView.swift
@@ -7,10 +7,32 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+	// Tracks how far the view has been dragged
+	@State private var offset = CGSize.zero
+	
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		 Text("Hello, World")
+			 .padding()
+			 .background(Color.blue)
+			 .foregroundColor(Color.white)
+			 .offset(offset)
+			 .gesture(
+				DragGesture()
+					.onChanged { gesture in
+						offset = gesture.translation
+					}
+					.onEnded { _ in
+						if abs(offset.height) > 200 {
+							offset = offset
+						} else {
+							withAnimation(.easeOut) {
+								offset = .zero
+							}
+						}
+				 }
+		  )
     }
 }
 
