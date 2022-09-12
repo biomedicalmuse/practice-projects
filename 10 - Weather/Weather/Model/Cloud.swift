58a46f61f399da035c8e35d33587c0f045963a5e
Natasha Godwin <natashasgodwin@gmail.com>
2022-09-12 12:24:24 -0400

    Edited the model

--- 10 - Weather/Weather/Model/Cloud.swift
+++ 10 - Weather/Weather/Model/Cloud.swift
@@ -2,32 +2,29 @@
 //  Cloud.swift
 //  Weather
 //
-//  Created by Natasha Godwin on 1/16/22.
+//  Created by Natasha Godwin on 9/12/22.
 //
 
 import SwiftUI
 
+/// A single cloud 
 class Cloud {
-	var position: CGPoint
-	let imageNumber: Int
-	let speed = Double.random(in: 4...12)
-	let scale: Double
+	 var position: CGPoint
+	 let imageNumber: Int
+	 let speed = Double.random(in: 4...12)
+	 let scale: Double
 	
-	init(imageNumber: Int, scale: Double) {
-		self.imageNumber = imageNumber
-		self.scale = scale
-		
-		let startX = Double.random(in: -400...400)
-		let startY = Double.random(in: -50...200)
-	   position = CGPoint(x: startX, y: startY)
+	enum Thickness {
+		 case none, thin, light, regular, thick, ultra
 	}
 	
-	enum Thickness: CaseIterable {
-		case none
-		case thin
-		case light
-		case regular
-		case thick
-		case ultra
+	init(imageNumber: Int, scale: Double) {
+		 self.imageNumber = imageNumber
+		 self.scale = scale
+
+		// Clouds move off-screen to create a wraparound effect when moving.
+		 let startX = Double.random(in: -400...400)
+		 let startY = Double.random(in: -50...200)
+		 position = CGPoint(x: startX, y: startY)
 	}
 }
--- 10 - Weather/Weather/Model/CloudGroup.swift
+++ 10 - Weather/Weather/Model/CloudGroup.swift
@@ -2,72 +2,63 @@
 //  CloudGroup.swift
 //  Weather
 //
-//  Created by Natasha Godwin on 1/16/22.
+//  Created by Natasha Godwin on 9/12/22.
 //
 
-import SwiftUI
+import Foundation
 
+
+/// A group of clouds whose number, opacity, and scale varies based on the weather.
 class CloudGroup {
-	var clouds = [Cloud]()
-	let opacity: Double
-	/// Stores updates to cloud positions
-	var lastUpdate = Date.now
+	 var clouds = [Cloud]()
+	 let opacity: Double
 	
-	
+	/*
+	 For any given level of cloud thickness, we’re going to adjust three values:
+	 
+    1. How many clouds to create.
+	 2. How translucent those clouds should be.
+	 3. How big or small the clouds can be.
+	 
+	 The number of clouds, opacity, and scale increases as thickness increases.
+	 
+	 */
 	init(thickness: Cloud.Thickness) {
-		let cloudsToCreate: Int
-		let cloudScale: ClosedRange<Double>
-		
-		switch thickness {
+		 let cloudsToCreate: Int
+		 let cloudScale: ClosedRange<Double>
+
+		 switch thickness {
 		 case .none:
-			cloudsToCreate = 0
-			opacity = 1
-			cloudScale = 1...1
+			  cloudsToCreate = 0
+			  opacity = 1
+			  cloudScale = 1...1
 		 case .thin:
-			cloudsToCreate = 10
-			opacity = 0.6
-			cloudScale = 0.2...0.4
+			  cloudsToCreate = 10
+			  opacity = 0.6
+			  cloudScale = 0.2...0.4
 		 case .light:
-			cloudsToCreate = 10
-			opacity = 0.7
-			cloudScale = 0.4...0.6
+			  cloudsToCreate = 10
+			  opacity = 0.7
+			  cloudScale = 0.4...0.6
 		 case .regular:
-			cloudsToCreate = 15
-			opacity = 0.8
-			cloudScale = 0.7...0.9
+			  cloudsToCreate = 15
+			  opacity = 0.8
+			  cloudScale = 0.7...0.9
 		 case .thick:
-			cloudsToCreate = 25
-			opacity = 0.9
-			cloudScale = 1.0...1.3
+			  cloudsToCreate = 25
+			  opacity = 0.9
+			  cloudScale = 1.0...1.3
 		 case .ultra:
-			cloudsToCreate = 40
-			opacity = 1
-			cloudScale = 1.2...1.6
-		}
-		
+			  cloudsToCreate = 40
+			  opacity = 1
+			  cloudScale = 1.2...1.6
+		 }
+
 		for i in 0..<cloudsToCreate {
-			let scale = Double.random(in: cloudScale)
-			let imageNumber = i % 8
-			let cloud = Cloud(imageNumber: imageNumber, scale: scale)
-			clouds.append(cloud)
+			 let scale = Double.random(in: cloudScale)
+			 let imageNumber = i % 8 // Decreases the chance of getting a duplicate image
+			 let cloud = Cloud(imageNumber: imageNumber, scale: scale)
+			 clouds.append(cloud)
 		}
-	}
-	
-	/// Figures out how much time has passed since the last uodate
-	/// Adjusts each cloud's position by that time, multiplied by the cloud's speed
-	/// - Parameter date: whatever day it is
-	func update(date: Date) {
-		let delta = date.timeIntervalSince1970 - lastUpdate.timeIntervalSince1970
-		
-		for cloud in clouds {
-			cloud.position.x -= delta * cloud.speed
-			let offScreenDistance = max(400, 400 * cloud.scale)
-			
-			if cloud.position.x < -offScreenDistance {
-				cloud.position.x = offScreenDistance
-			}
-		}
-		
-		lastUpdate = date
 	}
 }
