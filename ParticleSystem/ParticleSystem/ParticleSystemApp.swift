97ad0d4c1d823a90b930032925330afa9967d9ee
Natasha Godwin <natashasgodwin@gmail.com>
2022-09-24 16:59:33 -0400

    Added assets

---
+++ ParticleSystem/ParticleSystem/ParticleSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParticleSystemApp.swift
+//  ParticleSystem
+//
+//  Created by Natasha Godwin on 9/24/22.
+//
+
+import SwiftUI
+
+@main
+struct ParticleSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
